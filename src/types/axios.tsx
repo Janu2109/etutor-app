ee3dcd77126f9bb29207480938ff6c883876ca71
Janu Kleyn Fourie <s219295301@mandela.ac.za>
2022-08-19 21:11:40 +0200

    ADD API URL

--- src/types/axios.tsx
+++ src/types/axios.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 const instance = axios.create({
     // baseURL: 'https://localhost:7122/'
-    baseURL: 'https://localhost:7122/'
+    // baseURL: 'https://localhost:7122/'
     // baseURL: 'https://localhost:5001/'
+    baseURL: 'https://nmu-turbojs-api-live.azure-api.net/v2/'
 });
 export default instance;
